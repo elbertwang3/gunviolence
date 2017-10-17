40d9fe48edf3ac62c276a8af83e09f9dc987b467
elbertwang3 <elbert_wang@alumni.brown.edu>
2017-10-16 22:10:10 -0700

    legend working

--- js/massshootingmap.js
+++ js/massshootingmap.js
@@ -12,7 +12,9 @@ var projection = d3.geoAlbersUsa()
 
 var path = d3.geoPath()
    // .projection(projection);
-
+massshootingsvg.append("g")
+  .attr("class", "mslegend")
+  .attr("transform", "translate(" + 6*mswidth/7 + ", " + (30*msheight/50) + ")");
 var names = {};
 d3.queue()
     .defer(d3.json, "data/us.json")
@@ -67,6 +69,19 @@ function ready(error, us, statenames, shootings, populations) {
 	var colorScale = d3.scaleQuantile()
 						.domain(Object.values(nestedshootingsobj))
 						.range(d3.schemeBlues[9]);
+	
+	var mslegend = d3.legendColor()
+  //.shape('rectangle')
+  .labelFormat(d3.format(".2f"))
+
+  .title("Mass shootings per 1,000,000 people")
+    .titleWidth(150)
+  //use cellFilter to hide the "e" cell
+    .cellFilter(function(d){ return d.label !== "e" })
+  .scale(colorScale);
+
+    d3.select(".mslegend")
+  .call(mslegend);
 
 	console.log(d3.extent(shootings, function(d) { return +d.killed}))
 	var circleScale = d3.scaleLinear()
@@ -104,48 +119,6 @@ function ready(error, us, statenames, shootings, populations) {
 	      d.lat
 	    ]) + ")";
 	  });
-	/*massshootingsvg.append("defs").append("path")
-      .attr("id", "land")
-      .datum(topojson.feature(us, us.objects.land))
-      .attr("d", path);
-
-  	massshootingsvg.append("clipPath")
-      .attr("id", "clip-land")
-    .append("use")
-      .attr("xlink:href", "#land");*/
-
-	/*massshootingsvg.append("g")
-      .attr("class", "districts")
-      .attr("clip-path", "url(#clip-land)")
-    .selectAll("path")
-      .data(topojson.feature(congress, congress.objects.district).features)
-    .enter().append("path")
-      .attr("d", path)
-      .attr("fill", function(d) { 
-      
-        //console.log(names[+d.id]);
-        //console.log(unemployment.get(names[+d.id]));	
-        //console.log(color(unemployment.get(names[+d.id])));
-        //console.log(colorScale(nestedshootings[d.properties.GEOID]));
-        //console.log(nestedshootingsobj[d.properties.GEOID]);
-        return colorScale(nestedshootingsobj[d.properties.GEOID]);
-      })
-    .append("title")
-      .text(function(d) { return d.id; });
-
-    massshootingsvg.append("path")
-      .attr("class", "district-boundaries")
-      .datum(topojson.mesh(congress, congress.objects.district, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); }))
-      .attr("d", path);
-
-
-    massshootingsvg.append("path")
-      .attr("class", "state-boundaries")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("d", path);*/
-
- 
-
 }
 function add0ifneeded(id) {
 	return ('0' + id).slice(-2)
